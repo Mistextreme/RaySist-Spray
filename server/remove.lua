792ba92745cf139b6d704fd64b5cd888f838ecce
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-03-06 08:12:02 +0000

    Update remove.lua
    Esx conversion

--- server/remove.lua
+++ server/remove.lua
@@ -1,35 +1,31 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local ESX = exports['es_extended']:getSharedObject()
 
-QBCore.Functions.CreateUseableItem("spray_remover", function(source)
-    local source = source
-    local Player = QBCore.Functions.GetPlayer(source)
-    local item = Player.Functions.GetItemByName("spray_remover")
-    if item then
+ESX.RegisterUsableItem("spray_remover", function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local item = xPlayer.getInventoryItem("spray_remover")
+    
+    if item.count > 0 then
         TriggerClientEvent('RaySist-spray:removeClosestSpray', source)
-        TriggerClientEvent('QBCore:Notify', source, 'Stai rimuovendo lo spray!', "success")
+        TriggerClientEvent('esx:showNotification', source, 'Stai rimuovendo lo spray!')
     else
-        TriggerClientEvent('QBCore:Notify', source, 'Non hai un kit per rimuovere lo spray!', "error")
+        TriggerClientEvent('esx:showNotification', source, 'Non hai un kit per rimuovere lo spray!')
     end
 end)
 
-
 RegisterNetEvent('RaySist-spray:remove')
 AddEventHandler('RaySist-spray:remove', function(pos)
-    local Source = source
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local item = xPlayer.getInventoryItem("spray_remover")
 
-    local xPlayer = QBCore.Functions.GetPlayer(Source)
-    local item = xPlayer.Functions.GetItemByName("spray_remover")
-
-    if item then
-        xPlayer.Functions.RemoveItem("spray_remover", 1)
+    if item.count > 0 then
+        xPlayer.removeInventoryItem("spray_remover", 1)
         local sprayAtCoords = GetSprayAtCoords(pos)
 
-        MySQL.Async.execute([[
-            DELETE FROM sprays WHERE x=@x AND y=@y AND z=@z LIMIT 1
-        ]], {
+        MySQL.Async.execute('DELETE FROM sprays WHERE x = @x AND y = @y AND z = @z LIMIT 1', {
             ['@x'] = pos.x,
             ['@y'] = pos.y,
-            ['@z'] = pos.z,
+            ['@z'] = pos.z
         })
 
         for idx, s in pairs(SPRAYS) do
@@ -44,7 +40,7 @@ AddEventHandler('RaySist-spray:remove', function(pos)
         -- ensure someone doesnt bug it so its trying to remove other tags
         -- while deducting loyalty from not-deleted-but-at-coords tag
         if sprayAtCoords and not sprayAtCoordsAfterRemoval then
-            TriggerEvent('RaySist-sprays:removeSpray', Source, sprayAtCoords.text, sprayAtCoords.location)
+            TriggerEvent('RaySist-sprays:removeSpray', source, sprayAtCoords.text, sprayAtCoords.location)
         end
     end
 end)
