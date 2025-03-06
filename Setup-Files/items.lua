39e99eb33e6ed92700be324aae213a5c7f7e710c
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-03-06 08:18:05 +0000

    Update items.lua
    Ox inventory format

--- Setup-Files/items.lua
+++ Setup-Files/items.lua
@@ -1,26 +1,16 @@
-items = {
     ["spray"] = {
         name = "spray",
         label = "Spraycan",
         weight = 300,
-        type = "item",
-        image = "spray.png",
-        unique = false,
-        useable = true,
-        shouldClose = false,
-        combinable = nil,
-        description = "A spray can of paint"
+        stack = true,
+        close = false,
+        description = "A spray can of paint",
     },
     ["spray_remover"] = {
         name = "spray_remover",
         label = "Spray Removal Kit",
         weight = 300,
-        type = "item",
-        image = "spray_remover.png",
-        unique = false,
-        useable = true,
-        shouldClose = true,
-        combinable = nil,
-        description = "A spray paint removal kit"
-    }
-}
+        stack = true,
+        close = true,
+        description = "A spray paint removal kit",
+    },
